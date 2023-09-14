e076a46d3c40df07f88ffbc36170361e2ede7c09
jthomas47 <jackspurthomas@gmail.com>
2023-09-14 18:07:48 -0500

    set up DELETE route to delete a product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -34,7 +34,8 @@ router.get('/:id', async (req, res) => {
     });
     
     if (!productData) {
-      res.status(404).json({ message: 'No product with that id!' }); 
+      res.status(404).json({ message: 'No product found with that id!' }); 
+      return;
     }
 
     res.status(200).json(productData);
@@ -120,8 +121,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No Product found with that id!' });
+      return;
+    }
+
+    res.status(200).json(productData); 
+  } catch (err) {
+    res.status(500).json(err); 
+  }
 });
 
 module.exports = router;
